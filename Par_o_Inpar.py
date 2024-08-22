ff397eb385cf9e0f8c9b733b9d4e665b4cf619f8
Luis <luisjop7@gmail.com>
2024-08-22 15:46:45 -0600

    numero par o inpar codigo python

---
+++ Par_o_Inpar.py
@@ -0,0 +1,13 @@
+def par_o_inpar():
+    for numero in range (0,101,2):
+        print(numero)
+       
+par_o_inpar()
+print("listo _________________________")
+def par ():
+    for numero in range (0,101):
+        if numero % 2 == 0:
+            print(numero)
+            
+par()
+print("listo_________________")
